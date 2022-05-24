a2e1f076ade66fd9944b2dba329758c597784e01
Eggdropthebeat <chaejunhyeok@gmail.com>
2022-05-24 20:28:35 +0900

    기본기능

---
+++ app/src/main/java/com/example/recipify/NaviBar.java
@@ -0,0 +1,22 @@
+package com.example.recipify;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class NaviBar extends AppCompatActivity {
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_bar);
+
+
+    }
+}
