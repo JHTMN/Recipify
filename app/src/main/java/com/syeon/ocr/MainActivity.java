a1853315f441c127093ed55737ab6bfa98aa6d3f
Soyeon <swan79513@naver.com>
2022-06-05 17:53:33 +0900

    commit 10 layout change

--- app/src/main/java/com/syeon/ocr/MainActivity.java
+++ app/src/main/java/com/syeon/ocr/MainActivity.java
@@ -1,14 +1,19 @@
 package com.syeon.ocr;
 
+import static android.view.View.GONE;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.syeon.ocr.databinding.ActivityMainBinding;
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements OcrFragment.OcrFr
     private ActivityMainBinding activityMainBinding;
     public final int REQUEST_CODE_PERMISSIONS = 100; //카메라 권한설정
 
+    Context context;
     public static NoteDatabase noteDatabase = null;
 
 
@@ -39,6 +45,18 @@ public class MainActivity extends AppCompatActivity implements OcrFragment.OcrFr
         }else {
             requestPermissions(new String[] {Manifest.permission.CAMERA}, REQUEST_CODE_PERMISSIONS);
         }
+
+/*        activityMainBinding.saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                saveToDo();
+
+                Toast.makeText(getApplicationContext(),"추가되었습니다.",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        openDatabase();*/
 
     }
 
@@ -65,13 +83,15 @@ public class MainActivity extends AppCompatActivity implements OcrFragment.OcrFr
             if(ingredientList == null) { //식재료 값이 없으면 다시 인식
                 Toast.makeText(getApplicationContext(), "영수증을 다시 인식시켜주세요", Toast.LENGTH_SHORT).show();
                 getSupportFragmentManager().beginTransaction().remove(frag).commit();
-                activityMainBinding.OCRBtn.setVisibility(View.GONE);
+                activityMainBinding.OCRBtn.setVisibility(GONE);
                 OcrFragment ocrFragment = OcrFragment.newInstance();
                 ocrFragment.setOcrFragmentListener(MainActivity.this::ocrSuccess);
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 fragmentManager.beginTransaction().replace(R.id.main_container, ocrFragment, "OcrFragment")
                         .commit();
             } else { //식재료 값이 있을 경우 ingredientFragment 열기
+                activityMainBinding.noteView.setVisibility(View.VISIBLE);
+                activityMainBinding.OCRBtn.setVisibility(View.VISIBLE);
                 IngredientFragment ingredientFragment = IngredientFragment.newInstance(ingredientList);
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 fragmentManager.beginTransaction().replace(R.id.main_container, ingredientFragment, "OcrFragment")
@@ -91,7 +111,8 @@ public class MainActivity extends AppCompatActivity implements OcrFragment.OcrFr
         activityMainBinding.OCRBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                activityMainBinding.OCRBtn.setVisibility(View.GONE);
+                activityMainBinding.noteView.setVisibility(GONE);
+                activityMainBinding.OCRBtn.setVisibility(GONE);
                 OcrFragment ocrFragment = OcrFragment.newInstance();
                 ocrFragment.setOcrFragmentListener(MainActivity.this::ocrSuccess);
                 FragmentManager fragmentManager = getSupportFragmentManager();
@@ -101,18 +122,48 @@ public class MainActivity extends AppCompatActivity implements OcrFragment.OcrFr
         });
     }
 
-    /*
-    private void startIngredientBtn(){
-        activityMainBinding.OCRBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                activityMainBinding.OCRBtn.setVisibility(View.GONE);
-                IngredientFragment ingredientFragment = IngredientFragment.newInstance();
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.main_container, ingredientFragment)
-                        .commit();
-            }
-        });
-    }*/
+    private void saveToDo(){
+
+        //EditText에 적힌 글을 가져오기
+        String todo = activityMainBinding.inputToDo.getText().toString();
+
+        //테이블에 값을 추가하는 sql구문 insert...
+        String sqlSave = "insert into " + NoteDatabase.TABLE_NOTE + " (TODO) values (" +
+                "'" + todo + "')";
+
+        //sql문 실행
+        NoteDatabase database = NoteDatabase.getInstance(context);
+        database.execSQL(sqlSave);
+
+        //저장과 동시에 EditText 안의 글 초기화
+        activityMainBinding.inputToDo.setText("");
+    }
+
+
+    public void openDatabase() {
+        // open database
+        if (noteDatabase != null) {
+            noteDatabase.close();
+            noteDatabase = null;
+        }
+
+        noteDatabase = NoteDatabase.getInstance(this);
+        boolean isOpen = noteDatabase.open();
+        if (isOpen) {
+            Log.d(TAG, "Note database is open.");
+        } else {
+            Log.d(TAG, "Note database is not open.");
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (noteDatabase != null) {
+            noteDatabase.close();
+            noteDatabase = null;
+        }
+    }
+
 
 }
